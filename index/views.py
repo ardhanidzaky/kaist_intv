3808780c847c11479cc18bcbf3337a410978b9e7
ardhanidzaky <82014329+ardhanidzaky@users.noreply.github.com>
2023-05-12 21:20:09 +0700

    New FE

--- index/views.py
+++ index/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from .forms import MyForm
 from .controller import base_router
 
 import pandas as pd
+import json
 
 def index(request):
     if request.method == 'POST':
         form = MyForm(request.POST, request.FILES)
         if form.is_valid():
             text_input = form.cleaned_data['text_input']
+            request.session['vega_lite_spec'] = text_input
             checkbox = form.cleaned_data['check_box']
 
             if checkbox:
@@ -17,8 +20,8 @@ def index(request):
                 base_router(text_input=text_input, check_box=checkbox, data=csv_df)
             else:
                 base_router(text_input=text_input, check_box=checkbox)
-                
             return redirect('index')
     else:
         form = MyForm()
-    return render(request, 'index.html', {'form': form})
+        vega_lite_spec = request.session.get('vega_lite_spec', None)  # Retrieve the spec from the session
+        return render(request, 'index.html', {'form': form, 'vega_lite_spec': vega_lite_spec})
--- kaist_intv/settings.py
+++ kaist_intv/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
+SESSION_COOKIE_HTTPONLY = True
