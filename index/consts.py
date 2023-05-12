b9bdc10d523a6dd8240c2ca59bb42cae0f4b9480
ardhanidzaky <82014329+ardhanidzaky@users.noreply.github.com>
2023-05-12 13:27:48 +0700

    Add encoding for timeUnit

--- index/consts.py
+++ index/consts.py
@@ -5,3 +5,11 @@ VEGA_ENCODING = [
     'field', 'format', 'header', 'impute', 'legend', 'scale', 
     'stack', 'sort', 'timeUnit', 'type', 'value'
 ]
+
+VEGA_ENCODING_TIMEUNIT_DICT = {
+    'year': 'Y',
+    'quarter': 'Q',
+    'month': 'M',
+    'date': 'D',
+    'week': 'W'
+}
