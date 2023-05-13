a7c74dae1ad58120cde295ebf5a13c81c087f6cf
ardhanidzaky <82014329+ardhanidzaky@users.noreply.github.com>
2023-05-13 14:35:31 +0700

    Show rendered chart using Altair

--- index/views.py
+++ index/views.py
@@ -4,6 +4,9 @@ from .controller import base_router
 from .utils import update_data
 
 import pandas as pd
+import altair as alt
+import altair_viewer
+import json
 
 def index(request):
     if request.method == 'POST':
@@ -15,13 +18,14 @@ def index(request):
             if checkbox:
                 csv_upload = form.cleaned_data['csv_upload']
                 csv_df = pd.read_csv(csv_upload)
-                request.session['vega_lite_spec'] = update_data(text_input, csv_df)
+                chart = alt.Chart.from_json(update_data(text_input, csv_df))
+                chart = altair_viewer.display(chart)
                 return base_router(text_input=text_input, check_box=checkbox, data=csv_df)
             else:
-                request.session['vega_lite_spec'] = text_input
+                chart = alt.Chart.from_json(text_input)
+                chart = altair_viewer.display(chart)
                 return base_router(text_input=text_input, check_box=checkbox)
         return redirect('index')
     else:
         form = MyForm()
-        vega_lite_spec = request.session.get('vega_lite_spec', None)  # Retrieve the spec from the session
-        return render(request, 'index.html', {'form': form, 'vega_lite_spec': vega_lite_spec})
+        return render(request, 'index.html', {'form': form})
